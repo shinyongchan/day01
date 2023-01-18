d0e257ceb035e7cb77a20b3da6162cf7202c4043
syc <tlsdydcks135@naver.com>
2023-01-18 17:43:12 +0900

    ex 3.14

--- ex.py
+++ ex.py
@@ -1,19 +1,8 @@
-#ex 3.4
+#ex 3.14
 
-number = list(map(int, input("세 정수를 입력하시오 : ").split(' ')))
+letter = input("알파벳을 입력하시오 : ")
 
-if number [0] > number [1] :
-    if number [0] > number [2] and number[1] > number [2]:
-        print(number[0], number[1], number[2],)
-    else :
-        print(number[0], number[2], number[1], )
-elif number [1] > number[2] :
-    if number [2] > number[0] :
-        print(number[1], number[2], number[0], )
-    else :
-        print(number[1], number[0], number[2], )
+if letter in 'aeiou' :
+    print(f'{letter} (은)는 모음입니다.')
 else :
-    if number [0] > number [1] :
-        print(number[2], number[0], number[1], )
-    else :
-        print(number[2], number[1], number[0], )
+    print(f'{letter} (은)는 자음입니다.')
