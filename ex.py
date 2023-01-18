e0f0f3ed04f3ac264d0e1d849d0d91ec0346703a
syc <tlsdydcks135@naver.com>
2023-01-18 17:10:54 +0900

    ex 3.3

---
+++ ex.py
@@ -0,0 +1,12 @@
+#ex 3.3
+
+age = int(input("나이를 입력하시오 : "))
+heigt = int(input("키를 입력하시오(단위 cm) : "))
+
+if age >= 19 and heigt>=150 :
+    print("입장할 수 있습니다.")
+else :
+    print("입장할 수 없습니다.")
+
+
+
