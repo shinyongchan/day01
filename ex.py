0de313f0c52cd66c65c52f55fcbae085321b2e91
syc <tlsdydcks135@naver.com>
2023-01-18 17:38:33 +0900

    ex 3.4

--- ex.py
+++ ex.py
@@ -1,10 +1,19 @@
 #ex 3.4
 
-age = int(input("나이를 입력하시오 : "))
+number = list(map(int, input("세 정수를 입력하시오 : ").split(' ')))
 
-if age >=20 :
-    print("adult")
-elif age >= 10 :
-    print("youth")
+if number [0] > number [1] :
+    if number [0] > number [2] and number[1] > number [2]:
+        print(number[0], number[1], number[2],)
+    else :
+        print(number[0], number[2], number[1], )
+elif number [1] > number[2] :
+    if number [2] > number[0] :
+        print(number[1], number[2], number[0], )
+    else :
+        print(number[1], number[0], number[2], )
 else :
-    print("kid")
+    if number [0] > number [1] :
+        print(number[2], number[0], number[1], )
+    else :
+        print(number[2], number[1], number[0], )
