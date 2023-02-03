b14ce60d77677b7d60afdd4e7bc660b49dedd391
syc <tlsdydcks135@naver.com>
2023-02-03 11:16:26 +0900

    day15 linear list 02

--- day15.py
+++ day15.py
@@ -1,16 +1,26 @@
-pokemons = list()
+pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "이상해씨"]
 
 
-def add_data(pokemon):
-    pokemons.append(None)
-    #len_pokomons = len(pokemons)
-    pokemons[- 1] = pokemon
+def insert_data(idx, pokemon):
+    if idx < 0 or idx > len(pokemons):
+        print("데이터를 삽입할 범위를 벗어났습니다.")
+        return
+
+    pokemons.append(None)  # 빈칸 추가
 
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('이상해씨')
+    for i in range(len(pokemons) - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
 
-print(pokemons)
+    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
+
+if __name__ == "__main__":
+
+    insert_data(2, '거북왕')
+    print(pokemons)
+    insert_data(6, '어니부기')
+    print(pokemons)
+
+
+
