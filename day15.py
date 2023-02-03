0ea76f07218051b8811f29eb0ee45a3d8d19dbad
syc <tlsdydcks135@naver.com>
2023-02-03 14:13:21 +0900

    day15 linear list 05

--- day15.py
+++ day15.py
@@ -1,69 +1,51 @@
+## 함수 선언 부분 ##
+def print_poly(px):
+    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+    polyStr = "P(x) = "
 
+    for i in range(len(px)):
+        coef = px[i]  # 계수
 
-def add_data(pokemon):
-    pokemons.append(None)
-    pokemons[len(pokemons)-1] = pokemon
-
-def insert_data(idx, pokemon):
-    if idx < 0 or idx > len(pokemons):
-        print("데이터를 삽입할 범위를 벗어났습니다.")
-        return
-
-    pokemons.append(None)  # 빈칸 추가
-
-
-    for i in range(len(pokemons) - 1, idx, -1):
-        pokemons[i] = pokemons[i - 1]
-        pokemons[i - 1] = None
-
-    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
-
-def delete_data(idx):
-    if idx < 0 or idx > len(pokemons):
-        print("데이터를 삭제할 범위를 벗어났습니다.")
-        return
-
-    len_pokemons = len(pokemons)
-
-    pokemons[idx] = None  # 데이터 삭제
-    for i in range(idx + 1, len_pokemons):
-        pokemons[i - 1] = pokemons[i]
-        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
-    # for i in range(idx + 1, len_pokemons):
-    #     pokemons[i - 1] = pokemons[i]
-    #     pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
-
-    del (pokemons[len_pokemons - 1])
-
-pokemons = []
-menu =-1
-
-if __name__ == "__main__":
-    while (menu != 4):
-
-        menu = int(input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> "))
-
-        if (menu == 1):
-            data = input("추가할 데이터--> ")
-            add_data(data)
-            print(pokemons)
-        elif (menu == 2):
-            idx = int(input("삽입할 위치--> "))
-            data = input("추가할 데이터--> ")
-            insert_data(idx, data)
-            print(pokemons)
-        elif (menu == 3):
-            idx = int(input("삭제할 위치--> "))
-            delete_data(idx)
-            print(pokemons)
-        elif (menu == 4):
-            print(pokemons)
-            exit
+        if (coef > 0 and i > 0):
+            polyStr += "+"
+            polyStr += f'{coef}x^{term}'
+            term -= 1
+        elif (coef < 0 or i == 0):
+            polyStr += f'{coef}x^{term}'
+            term -= 1
         else:
-            print("1~4 중 하나를 입력하세요.")
-            continue
+            term -= 1
 
 
 
+
+
+
+    return polyStr
+
+
+def calc_poly(xValue, px):
+    retValue = 0
+    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+
+    for i in range(len(px)):
+        coef = px[i]  # 계수
+        retValue += coef * xValue ** term
+        term -= 1
+
+    return retValue
+
+
+## 전역 변수 선언 부분 ##
+px = [7, -4, 6, 2]  # =
+
+## 메인 코드 부분 ##
+if __name__ == "__main__":
+    pStr = print_poly(px)
+    print(pStr)
+
+    xValue = int(input("X 값 : "))
+
+    print(calc_poly(xValue, px))
 
 
