5abca5f6984d3d2386ca70f297d93e0521a6bcb1
syc <tlsdydcks135@naver.com>
2023-02-03 14:26:23 +0900

    day15 linear list 06

--- day15.py
+++ day15.py
@@ -1,20 +1,19 @@
 ## 함수 선언 부분 ##
-def print_poly(px):
-    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+def print_poly(px, tx):
+                             # 최고차항 숫자 = 배열길이-1
     polyStr = "P(x) = "
 
     for i in range(len(px)):
         coef = px[i]  # 계수
-
+        term = tx[i]
         if (coef > 0 and i > 0):
-            polyStr += "+"
+            polyStr += f'+{coef}x^{term}'
+
+        elif (coef < 0 or i == 0) :
             polyStr += f'{coef}x^{term}'
-            term -= 1
-        elif (coef < 0 or i == 0):
-            polyStr += f'{coef}x^{term}'
-            term -= 1
-        else:
-            term -= 1
+
+
+
 
 
 
@@ -24,28 +23,29 @@ def print_poly(px):
     return polyStr
 
 
-def calc_poly(xValue, px):
+def calc_poly(xValue, px, tx):
     retValue = 0
-    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+    # 최고차항 숫자 = 배열길이-1
 
     for i in range(len(px)):
         coef = px[i]  # 계수
+        term = tx[i]
         retValue += coef * xValue ** term
-        term -= 1
 
     return retValue
 
 
 ## 전역 변수 선언 부분 ##
-px = [7, -4, 6, 2]  # =
+px = [7, -4, 5]  # =
+tx = [300, 20, 0]
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__":
-    pStr = print_poly(px)
+    pStr = print_poly(px,tx)
     print(pStr)
 
     xValue = int(input("X 값 : "))
 
-    print(calc_poly(xValue, px))
+    print(calc_poly(xValue, px, tx))
 
 
