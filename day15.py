32b2d75c281f6d92bed21ca095f93997bad1718d
syc <tlsdydcks135@naver.com>
2023-02-03 11:30:05 +0900

    day15 linear list 03

--- day15.py
+++ day15.py
@@ -15,11 +15,27 @@ def insert_data(idx, pokemon):
 
     pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
 
+def delete_data(idx):
+    if idx < 0 or idx > len(pokemons):
+        print("데이터를 삭제할 범위를 벗어났습니다.")
+        return
+
+    len_pokemons = len(pokemons)
+    pokemons[idx] = None  # 데이터 삭제
+
+    for i in range(idx + 1, len_pokemons):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+
+    del (pokemons[len_pokemons - 1])
+
 if __name__ == "__main__":
 
-    insert_data(2, '거북왕')
+    delete_data(1)
+    #insert_data(2, '거북왕')
     print(pokemons)
-    insert_data(6, '어니부기')
+    delete_data(3)
+    #insert_data(6, '어니부기')
     print(pokemons)
 
 
